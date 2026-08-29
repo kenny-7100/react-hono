2dea4d7e5ee65f9f0ddc878976e3ccffc39705d3
kenny-7100 <233663230+kenny-7100@users.noreply.github.com>
2026-08-29 12:55:09 +0800

    simplify not found page

--- ui/src/pages/NotFound/index.tsx
+++ ui/src/pages/NotFound/index.tsx
@@ -1,15 +1,7 @@
-import { Link } from 'react-router-dom';
 import styles from './index.module.scss';
 
 function NotFound() {
-  return (
-    <main className={styles.page}>
-      <p className={styles.eyebrow}>404</p>
-      <h1>Page not found</h1>
-      <p>The page you requested does not exist.</p>
-      <Link className={styles.pageLink} to="/">Return home</Link>
-    </main>
-  );
+  return <main className={styles.page}>404</main>;
 }
 
 export { NotFound as Component };

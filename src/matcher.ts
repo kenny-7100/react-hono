9cb362c081e7a5d3937233e735594fd944d5656b
kenny-7100 <233663230+kenny-7100@users.noreply.github.com>
2026-08-29 19:46:13 +0800

    Add matcher result types

--- src/matcher.ts
+++ src/matcher.ts
@@ -6,6 +6,16 @@ interface Order {
   amount: bigint;
 }
 
+interface Dealt {
+  amount: bigint;
+  filledOrders: Order[];
+}
+
+interface LimitResult {
+  dealt?: Dealt;
+  order?: Order;
+}
+
 class Matcher {
   private askOrderList: Order[] = [];
   private bidOrderList: Order[] = [];
@@ -20,7 +30,7 @@ class Matcher {
     }
   }
 
-  private limitBid(bidOrder: Order) {
+  private limitBid(bidOrder: Order): LimitResult {
     const order = { ...bidOrder };
     const filledOrders: Order[] = [];
     while (this.askOrderList.length > 0 && order.amount > 0n && order.price >= this.askOrderList[0].price) {
@@ -44,7 +54,7 @@ class Matcher {
     return dealt.amount === 0n ? { order } : { order, dealt };
   }
 
-  private limitAsk(askOrder: Order) {
+  private limitAsk(askOrder: Order): LimitResult {
     const order = { ...askOrder };
     const filledOrders: Order[] = [];
     while (this.bidOrderList.length > 0 && order.amount > 0n && order.price <= this.bidOrderList[0].price) {
@@ -78,7 +88,7 @@ class Matcher {
     }
   }
 
-  private marketBuy(buyOrder: Order) {
+  private marketBuy(buyOrder: Order): Dealt {
     const filledOrders: Order[] = [];
     let buyAmount = buyOrder.amount;
     while (this.askOrderList.length > 0 && buyAmount > 0n) {
@@ -95,7 +105,7 @@ class Matcher {
     return { amount: buyOrder.amount - buyAmount, filledOrders };
   }
 
-  private marketSell(sellOrder: Order) {
+  private marketSell(sellOrder: Order): Dealt {
     const filledOrders: Order[] = [];
     let sellAmount = sellOrder.amount;
     while (this.bidOrderList.length > 0 && sellAmount > 0n) {

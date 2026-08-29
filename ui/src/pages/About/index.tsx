ec5c843b25a46d5fa280153f1dd150dca3e9537d
kenny-7100 <233663230+kenny-7100@users.noreply.github.com>
2026-08-29 12:57:59 +0800

    simplify about page

--- ui/src/pages/About/index.tsx
+++ ui/src/pages/About/index.tsx
@@ -1,13 +1,7 @@
 import styles from './index.module.scss';
 
 function About() {
-  return (
-    <main className={styles.page}>
-      <p className={styles.eyebrow}>About</p>
-      <h1>React meets Hono</h1>
-      <p>This project combines a Vite-powered React interface with a Hono API running on Cloudflare Workers.</p>
-    </main>
-  );
+  return <main className={styles.page}>About</main>;
 }
 
 export { About as Component };

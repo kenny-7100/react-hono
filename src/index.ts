abd30f740d6a851b9bb0db93b636a778f16bb3e6
kenny-7100 <233663230+kenny-7100@users.noreply.github.com>
2026-09-09 13:06:34 +0800

    feat: puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 import { Hono } from 'hono';
+import puppeteer from '@cloudflare/puppeteer';
 
-const app = new Hono();
+type Bindings = {
+  BROWSER: Fetcher;
+};
 
-app.get('/api/hello', (c) => {
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.get('/api/hello', async (c) => {
+  const browser = await puppeteer.launch(c.env.BROWSER);
+
+  try {
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com/', { waitUntil: 'networkidle2' });
+  } finally {
+    await browser.close();
+  }
+
   return c.json({ message: 'Hello Hono!' });
 });
 
